5affea858e40306214f9f52ca9a120046b216f1a
lontonggg <zadareyhan@gmail.com>
2025-06-28 16:06:19 +0700

    fix: revise navbar

--- src/components/navigations/Navbar.tsx
+++ src/components/navigations/Navbar.tsx
@@ -20,11 +20,11 @@ export const Navbar = () => {
       transition={{ duration: 1.2, ease: "easeOut" }}
     >
       <motion.div 
-        className="bg-black/5 backdrop-blur-md border border-white/10 shadow-lg rounded-full px-6 py-3 mt-4"
+        className="bg-black/5 backdrop-blur-md border border-white/10 shadow-lg rounded-full px-4 md:px-6 py-2 md:py-3 lg:mt-4"
         whileHover={{ scale: 1.05 }}
         transition={{ duration: 0.2, ease: "easeOut" }}
       >
-        <div className="flex space-x-6 md:space-x-8 text-lg md:text-xl text-primary">
+        <div className="flex space-x-6 md:space-x-8 md:text-xl text-primary">
           <h1 
             className="transition-all duration-200 hover:text-gray-300 hover:scale-105 cursor-pointer px-4 py-2 rounded-full hover:bg-white/10"
             onClick={() => scrollToSection('about')}
